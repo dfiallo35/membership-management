ae103bbb2670ee78d1b984d459da4c02c65e8478
Dennis Fiallo <dfiallo35@gmail.com>
2025-10-15 19:51:57 -0400

    update db connection

--- features/membership/infrastructure/database/postgres.py
+++ features/membership/infrastructure/database/postgres.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from contextlib import asynccontextmanager
 from typing import AsyncGenerator
-from sqlalchemy import NullPool
+from sqlalchemy.pool import AsyncAdaptedQueuePool
 
 
 logger = logging.getLogger(__name__)
@@ -14,7 +14,12 @@ class DbConnection:
         if not db_url:
             raise Exception("Database URL is required")
 
-        self.engine = create_async_engine(db_url, poolclass=NullPool)
+        self.engine = create_async_engine(
+            db_url,
+            pool_size=5,
+            max_overflow=10,
+            poolclass=AsyncAdaptedQueuePool,
+        )
 
         self.session_factory = sessionmaker(
             bind=self.engine,
@@ -24,13 +29,11 @@ class DbConnection:
 
     @asynccontextmanager
     async def get_session(self) -> AsyncGenerator[AsyncSession, None]:
-        session = self.session_factory()
-        try:
-            yield session
-            await session.commit()
-        except Exception as e:
-            logger.error(e)
-            await session.rollback()
-            raise e
-        finally:
-            await session.close()
+        async with self.session_factory() as session:
+            try:
+                yield session
+                await session.commit()
+            except Exception as e:
+                await session.rollback()
+                logger.exception("Database transaction failed: %s", e)
+                raise
